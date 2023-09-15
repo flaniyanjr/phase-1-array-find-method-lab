62d9debd9c1819a526aeadadb3df655fa08a19af
Felix Laniyan <flaniyanjr@gmail.com>
2023-09-14 22:54:28 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(seasonResults) {
+    let obj= seasonResults.find( (yearOutcome) => {
+        if (yearOutcome.result === 'W') {
+            return yearOutcome
+        }
+    })
+    if (obj) {
+        return obj.year
+    }
+}
